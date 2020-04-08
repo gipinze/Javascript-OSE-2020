c4e8b5aa964ec70825c79172a5edc4327911e282
Gianmarco Ipinze <48326351+gipinze@users.noreply.github.com>
2020-04-08 17:38:13 +0300

    Add files via upload

--- 09.007/09.07ImagesEx.js
+++ 09.007/09.07ImagesEx.js
@@ -1,11 +1,8 @@
-// Create an array with file names of images
 var images = ["face.png", "heart.png", "sun.png", "moon.png", "cloud.png"];
-var text ="";
+var text = "";
 
-function addImages() {
-  // Use a for loop to show all the images. 
-  for (var i=0; i<images.lenght; i++) {
-  }
-// Use the document.write() method to write one new image element to the web page per each image.
-	document.write("<img src =" + images [i] + " alt= 'Funny shape'>");
-}
+function addImages(){
+	for (var i = 0; i < images.length; i++)
+
+document.write("<img src=" + images[i] + " alt='Funny shape'>");
+		 }
